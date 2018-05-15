fd35453b45a57ce412c886f986f9ec60d0a880f3
harshitha1997 <38415405+harshitha1997@users.noreply.github.com>
2018-05-15 12:14:45 +0530

    Add files via upload

--- slot_book.sql
+++ slot_book.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 02, 2018 at 05:12 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Host: localhost:3306
+-- Generation Time: May 14, 2018 at 10:59 PM
+-- Server version: 5.7.21
+-- PHP Version: 7.1.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,15 +25,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(25) NOT NULL,
+  `unm` varchar(255) NOT NULL,
+  `pwd` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `unm`, `pwd`) VALUES
+(1, 'admin', 'hello');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `bkgs`
 --
 
 CREATE TABLE `bkgs` (
   `id` int(25) NOT NULL,
-  `slid` int(25) NOT NULL,
+  `crid` varchar(255) NOT NULL,
   `secid` int(25) NOT NULL,
-  `date` varchar(255) NOT NULL
+  `venue` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `period` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bkgs`
+--
+
+INSERT INTO `bkgs` (`id`, `crid`, `secid`, `venue`, `date`, `period`) VALUES
+(5, '2015cse036', 1, 'Tennis', '2018-05-14', 1),
+(6, '2015cse036', 1, 'Tennis', '2018-05-15', 1),
+(7, '2015cse036', 1, 'Badminton', '2018-05-16', 5),
+(8, '2015cse036', 1, 'Tennis', '2018-05-17', 1),
+(9, '2015cse036', 1, 'Badminton', '2018-05-17', 5);
 
 -- --------------------------------------------------------
 
@@ -83,16 +117,19 @@ INSERT INTO `sec` (`id`, `sec`) VALUES
 
 CREATE TABLE `slot` (
   `id` int(25) NOT NULL,
-  `slot` varchar(255) NOT NULL
+  `period` varchar(255) NOT NULL,
+  `from` varchar(255) NOT NULL,
+  `to` varchar(255) NOT NULL,
+  `secid` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `slot`
 --
 
-INSERT INTO `slot` (`id`, `slot`) VALUES
-(1, 'Cricket'),
-(2, 'Foot Ball');
+INSERT INTO `slot` (`id`, `period`, `from`, `to`, `secid`) VALUES
+(1, '1', '9', '10', 1),
+(2, '5', '3', '4', 1);
 
 -- --------------------------------------------------------
 
@@ -116,9 +153,34 @@ INSERT INTO `tt` (`id`, `secid`, `day`, `frm`, `to`) VALUES
 (1, 1, 'Wed', 4, 6),
 (2, 1, 'Thu', 4, 6);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venue`
+--
+
+CREATE TABLE `venue` (
+  `id` int(25) NOT NULL,
+  `slot` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `venue`
+--
+
+INSERT INTO `venue` (`id`, `slot`) VALUES
+(1, 'Tennis'),
+(2, 'Badminton');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `bkgs`
+--
+ALTER TABLE `bkgs`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `cr`
@@ -133,9 +195,21 @@ ALTER TABLE `sec`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `slot`
+--
+ALTER TABLE `slot`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tt`
 --
 ALTER TABLE `tt`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venue`
+--
+ALTER TABLE `venue`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -143,20 +217,42 @@ ALTER TABLE `tt`
 --
 
 --
+-- AUTO_INCREMENT for table `bkgs`
+--
+ALTER TABLE `bkgs`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `cr`
 --
 ALTER TABLE `cr`
   MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `sec`
 --
 ALTER TABLE `sec`
   MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `slot`
+--
+ALTER TABLE `slot`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `tt`
 --
 ALTER TABLE `tt`
   MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `venue`
+--
+ALTER TABLE `venue`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
